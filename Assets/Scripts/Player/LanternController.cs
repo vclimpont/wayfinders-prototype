e783233d4ef3fa4f8ed8018e1bdd84eb9f366693
vclimpont <46527505+vclimpont@users.noreply.github.com>
2020-11-21 17:28:32 +0100

    init project
    - created player controller basics movements
    - created lantern object (light and material)
    - created lantern controller

---
+++ Assets/Scripts/Player/LanternController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanternController : MonoBehaviour
+{
+    public Light lanternLight;
+    public MeshRenderer lanternRenderer;
+    public Material lanternMaterialOn;
+    public Material lanternMaterialOff;
+    public float fadingSpeed;
+
+    public bool Active { get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Active = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        FadeLight();
+    }
+
+    private void FadeLight()
+    {
+        if(lanternLight.isActiveAndEnabled && lanternLight.intensity > 0)
+        {
+            lanternLight.intensity -= fadingSpeed;
+        }
+    }
+
+    public void SwitchActive()
+    {
+        Active = !Active;
+        lanternLight.enabled = Active;
+        lanternRenderer.material = Active ? lanternMaterialOn : lanternMaterialOff;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody rb;
+    private LanternController lantern;
+
+    private float xMovement;
+    private float zMovement;
+        
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        lantern = GetComponent<LanternController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xMovement = Input.GetAxisRaw("Horizontal");
+        zMovement = Input.GetAxisRaw("Vertical");
+
+        if(Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("active");
+            lantern.SwitchActive();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        rb.velocity = new Vector3(xMovement * speed, rb.velocity.y, zMovement * speed);
+    }
+}
