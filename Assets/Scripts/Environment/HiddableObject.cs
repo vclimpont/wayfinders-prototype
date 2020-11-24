fba49b32efcdc0d300c709bc9ef1926362e70d0b
vclimpont <46527505+vclimpont@users.noreply.github.com>
2020-11-24 01:22:17 +0100

    hiddable objects with alpha fading

--- Assets/Scripts/Environment/HiddableObject.cs
+++ Assets/Scripts/Environment/HiddableObject.cs
@@ -4,16 +4,52 @@ using UnityEngine;
 
 public class HiddableObject : MonoBehaviour, IHiddable
 {
+    [SerializeField] private float chargeTime;
+
     public bool Hidden { get; set; }
+
+    private MeshRenderer mesh;
+    private float currentCharge;
+
+    private void Start()
+    {
+        mesh = GetComponent<MeshRenderer>();
+        currentCharge = 0;
+    }
+
+    private void SetMaterialAlpha(float a)
+    {
+        Color c = mesh.material.color;
+        mesh.material.color = new Color(c.r, c.g, c.b, a);
+    }
 
     public void Hide(bool hide)
     {
-        if(Hidden == hide)
+        if (hide)
         {
-            return;
+            if (currentCharge >= chargeTime)
+            {
+                Hidden = true;
+                mesh.enabled = false;
+            }
+        }
+        else
+        {
+            SetMaterialAlpha(1);
+            currentCharge = 0;
+            Hidden = false;
+            mesh.enabled = true;
         }
 
-        Hidden = hide;
-        gameObject.SetActive(!Hidden);
+        LoadHide();
+    }
+
+    public void LoadHide()
+    {
+        if (currentCharge <= chargeTime)
+        {
+            SetMaterialAlpha(1f - (currentCharge / chargeTime));
+            currentCharge += Time.deltaTime;
+        }
     }
 }
--- Assets/Scripts/Lantern/PurpleLantern.cs
+++ Assets/Scripts/Lantern/PurpleLantern.cs
@@ -9,22 +9,10 @@ public class PurpleLantern : LanternProperty
     private Color lightColor;
     private float detectRadius;
 
+    private Collider[] hitCollidersBfr;
     private Collider[] hitColliders;
 
-    private void FixedUpdate()
-    {
-        if (hitColliders != null)
-        {
-            foreach (Collider hit in hitColliders)
-            {
-                MonoBehaviour hitMB = hit.GetComponent<MonoBehaviour>();
-                if (hitMB is IHiddable hitHiddable)
-                {
-                    hitHiddable.Hide(true);
-                }
-            }
-        }
-    }
+    private Vector3 hitPoint;
 
     public override void UsePropertyAction(RaycastHit hitInfo)
     {
@@ -33,9 +21,78 @@ public class PurpleLantern : LanternProperty
             return;
         }
 
-        hitColliders = Physics.OverlapSphere(hitInfo.point, detectRadius);
+        ShowLanternLight(hitInfo);
 
-        if(pointObject == null)
+        hitColliders = Physics.OverlapSphere(hitInfo.point, detectRadius);
+        hitPoint = hitInfo.point;
+
+        HideObjects();
+        RestoreObjects();
+        hitCollidersBfr = hitColliders;
+    }
+
+    public override void StopPropertyAction()
+    {
+        HideObjects(false);
+        Destroy(pointObject);
+        hitColliders = null;
+        hitCollidersBfr = null;
+    }
+
+    public void SetProperties(Color col, float rad)
+    {
+        lightColor = col;
+        detectRadius = rad;
+    }
+
+    private void HideObjects(bool hide = true)
+    {
+        if (hitColliders != null)
+        {
+            foreach (Collider hit in hitColliders)
+            {
+                MonoBehaviour hitMB = hit.GetComponent<MonoBehaviour>();
+                if (hitMB is IHiddable hitHiddable)
+                {
+                    hitHiddable.Hide(hide);
+                }
+            }
+        }
+    }
+
+    private void RestoreObjects()
+    {
+        if(hitCollidersBfr != null)
+        {
+            foreach (Collider hitBfr in hitCollidersBfr)
+            {
+                if(!Contains(hitColliders, hitBfr))
+                {
+                    MonoBehaviour hitMB = hitBfr.GetComponent<MonoBehaviour>();
+                    if (hitMB is IHiddable hitHiddable)
+                    {
+                        hitHiddable.Hide(false);
+                    }
+                }
+            }
+        }
+    }
+
+    private bool Contains(Collider[] colls, Collider c)
+    {
+        for (int i = 0; i < colls.Length; i++)
+        {
+            if (colls[i].Equals(c))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void ShowLanternLight(RaycastHit hitInfo)
+    {
+        if (pointObject == null)
         {
             pointObject = new GameObject();
         }
@@ -46,26 +103,19 @@ public class PurpleLantern : LanternProperty
         }
     }
 
-    public override void StopPropertyAction()
-    {
-        Destroy(pointObject);
-        hitColliders = null;
-    }
-
-    public void SetProperties(Color col, float rad)
-    {
-        lightColor = col;
-        detectRadius = rad;
-    }
-
     private void SetLight()
     {
         pointLight = pointObject.AddComponent<Light>();
         pointLight.type = LightType.Point;
         pointLight.color = lightColor;
         pointLight.intensity = 5;
-        pointLight.range = detectRadius * 3f;
+        pointLight.range = detectRadius * 2f;
         pointLight.renderMode = LightRenderMode.ForcePixel;
         pointLight.bounceIntensity = 0;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(hitPoint, detectRadius);
     }
 }
